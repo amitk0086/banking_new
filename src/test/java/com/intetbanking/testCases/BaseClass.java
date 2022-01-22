6fd67f7f94de1e6c4ac5dfb4f09395b4e75026e8
Amit <amitk0086@gmail.com>
2022-01-22 10:05:55 +0530

    Added config.properties file

--- src/test/java/com/intetbanking/testCases/BaseClass.java
+++ src/test/java/com/intetbanking/testCases/BaseClass.java
@@ -1,5 +1,6 @@
 package com.intetbanking.testCases;
 
+import com.intetbanking.utilities.ReadConfig;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
@@ -11,16 +12,17 @@ import org.testng.annotations.BeforeClass;
 
 public class BaseClass {
 
-    public String baseurl="https://demo.guru99.com/v3/index.php";
-    public String username="mngr381956";
-    public String Password ="Yhenyze";
+    ReadConfig readConfig= new ReadConfig();
+    public String baseurl=readConfig.getApplicationURL();
+    public String username=readConfig.getUserName();
+    public String Password =readConfig.getPassword();
     public static WebDriver driver;
     public static Logger logger;
 
     @BeforeClass
     public void SetUp()
     {
-        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//Drivers//chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver",readConfig.getchromeDriverPath());
         driver= new ChromeDriver();
         logger= Logger.getLogger("ebanking");
         PropertyConfigurator.configure("log4j.properties");
